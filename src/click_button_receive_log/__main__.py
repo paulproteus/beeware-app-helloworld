5ce867a961ddf14411980455d3593d38f47aa128
Asheesh Laroia <asheesh@asheesh.org>
2020-03-24 21:43:57 -0700

    Stop calling ourselves helloworld

--- src/click_button_receive_log/__main__.py
+++ src/click_button_receive_log/__main__.py
@@ -1,6 +1,6 @@
-from helloworld.app import main
+from click_button_receive_log.app import main
 
-print('helloworld app __main__ executing')
+print('click_button_receive_log helloworld app __main__ executing')
 
 if __name__ == '__main__':
     main().main_loop()
